2bbd33dcd222f907adfd3352e2edd950b7afd3a7
EngAhmedAtef <84854766+EngAhmedAtef@users.noreply.github.com>
2023-10-23 14:16:55 +0300

    Minor changes

--- src/main/java/com/ahmedatef/springboot/restcrud/service/CalculatorService.java
+++ src/main/java/com/ahmedatef/springboot/restcrud/service/CalculatorService.java
@@ -5,20 +5,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class CalculatorService {
 
-    public int add(int x, int y) {
-        return x + y;
-    }
+    public int add(int x, int y) { return x + y; }
 
-    public int subtract(int x, int y) {
-        return x - y;
-    }
+    public int subtract(int x, int y) { return x - y; }
 
-    public int multiply(int x, int y) {
-        return x * y;
-    }
+    public int multiply(int x, int y) { return x * y; }
 
-    public int divide(int x, int y) {
-        return x / y;
-    }
+    public int divide(int x, int y) { return x / y; }
 
 }
--- src/test/java/com/ahmedatef/springboot/restcrud/CalculatorServiceTest.java
+++ src/test/java/com/ahmedatef/springboot/restcrud/CalculatorServiceTest.java
@@ -3,14 +3,21 @@ package com.ahmedatef.springboot.restcrud;
 import com.ahmedatef.springboot.restcrud.service.CalculatorService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class CalculatorServiceTest {
 
+    private final CalculatorService service;
+
+    @Autowired
+    public CalculatorServiceTest(CalculatorService service) {
+        this.service = service;
+    }
+
     @Test
-    public void ArithmeticOperationsService_Add_ReturnsCorrectAnswer() {
-
-        CalculatorService service = new CalculatorService();
-
+    public void CalculatorService_Add_ReturnsCorrectAnswer() {
         int answer = 4;
         int expected = service.add(2, 2);
 
@@ -19,9 +26,6 @@ public class CalculatorServiceTest {
 
     @Test
     public void CalculatorService_Subtract_ReturnsCorrectAnswer() {
-
-        CalculatorService service = new CalculatorService();
-
         int answer = 2;
         int expected = service.subtract(5, 3);
 
@@ -30,9 +34,6 @@ public class CalculatorServiceTest {
 
     @Test
     public void CalculatorService_Multiply_ReturnsCorrectAnswer() {
-
-        CalculatorService service = new CalculatorService();
-
         int answer = 15;
         int expected = service.multiply(5, 3);
 
@@ -41,9 +42,6 @@ public class CalculatorServiceTest {
 
     @Test
     public void CalculatorService_Divide_ReturnsCorrectAnswer() {
-
-        CalculatorService service = new CalculatorService();
-
         int answer = 5;
         int expected = service.divide(10, 2);
 
